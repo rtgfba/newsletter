60605e98442a9acb6e679d09d6b1745e6fd995fb
rtg <120173063+rtgfba@users.nonreply.github.com>
2023-02-22 15:55:07 -0500

    Update server

--- app.js
+++ app.js
@@ -6,12 +6,20 @@ const request = require("request");
 
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+
 
 app.get("/", function(req,res){
     res.sendFile(__dirname + "/signup.html");
 });
 
-
+app.post("/", function(req, res){
+    var firstName = req.body.fname;
+    var lastName = req.body.lname;
+    var email = req.body.email;
+    console.log(firstName, lastName, email);
+})
 
 
 
