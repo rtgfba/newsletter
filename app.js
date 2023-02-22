61fa8c144a224413ce8bb08ffaee2d3733dddc34
rtg <120173063+rtgfba@users.nonreply.github.com>
2023-02-21 20:57:51 -0500

    Test to see if server is online

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-const requests = require("requests");
+const request = require("request");
 
 
+
+
+
+app.get("/", function(req,res){
+    res.send("This server  is up and running");
+});
 
 
 
@@ -13,4 +19,4 @@ const requests = require("requests");
 
 app.listen(3000, function(){
     console.log("This server is running on port 3000");
-})
+});
