b0f3bb6f53c66f2f858d0cf84156af0610e5b17b
rtg <120173063+rtgfba@users.nonreply.github.com>
2023-02-21 20:44:29 -0500

    Init NPM, add request, body parser and express

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const requests = require("requests");
+
+
+
+
+
+
+
+
+
+app.listen(3000, function(){
+    console.log("This server is running on port 3000");
+})
