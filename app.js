3b37bcb92bc9b3ad5cc29eee5ece4ea2a3d1eb24
rtg <120173063+rtgfba@users.nonreply.github.com>
2023-02-22 18:24:05 -0500

    Create front end and backend functionality Linked up the front end to the back end of the newsletter server

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const request = require("request");
-
+const https = require("https");
+require('dotenv').config();
+const apiKey = process.env.API_KEY 
+const audienceKey = process.env.AUDIENCE_KEY
 
 
 app.use(express.static("public"));
@@ -15,12 +18,55 @@ app.get("/", function(req,res){
 });
 
 app.post("/", function(req, res){
-    var firstName = req.body.fname;
-    var lastName = req.body.lname;
-    var email = req.body.email;
-    console.log(firstName, lastName, email);
-})
+    const firstName = req.body.fname;
+    const lastName = req.body.lname;
+    const email = req.body.email;
+    
+    const data = {
+        members:[
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields:{
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    };
 
+    const jSonData = JSON.stringify(data);
+
+    const url = "https://us21.api.mailchimp.com/3.0/lists/"+ audienceKey +"";
+
+    const options = {
+        method: "POST",
+        auth: ""+ apiKey + ""
+    }
+
+
+
+    const request = https.request(url, options, function(response){
+        response.on("data", function(data){
+            console.log(JSON.parse(data));
+           if (response.statusCode === 200){
+            res.sendFile(__dirname + "/success.html");
+           } else {
+            res.sendFile(__dirname + "/failure.html");
+           }
+        });
+
+    });
+    
+    request.write(jSonData);
+    request.end();
+
+});
+
+
+app.post("/failure", function(req,res){
+    res.redirect("/");
+});
 
 
 
