75bb8580f5d41c86826430ef6611cc14a76055b7
rtg <120173063+rtgfba@user.nonreply.github.com>
2023-02-26 23:49:23 -0500

    space

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.get("/", function(req,res){
 app.post("/", function(req, res){ 
     const firstName = req.body.fname;
     const lastName = req.body.lname;
-    const email = req.body.email;
+    const email = req.body.email; 
     
     const data = {
         members:[
